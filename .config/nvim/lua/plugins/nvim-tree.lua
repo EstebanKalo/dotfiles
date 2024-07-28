f3326afab9d7929350d5abe8bb48d837dead0bcc
caloway <tebi_cba1@hotmail.com>
2024-07-28 00:36:45 -0300

    update right and left

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,6 +1,19 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+  on_attach = function(bufnr)
+    local api = require('nvim-tree.api')
+
+    local function opts(desc)
+      return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
+
+    -- Mapeos de teclas personalizados
+    vim.keymap.set('n', '<Left>', api.node.navigate.parent_close, opts('Close Directory'))
+    vim.keymap.set('n', '<Right>', api.node.open.edit, opts('Open'))
+  end
+})
 
 vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
+
