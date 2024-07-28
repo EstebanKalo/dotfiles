2ebd5d90cc545ac9b0968496caa0c4c72773e34c
caloway <tebi_cba1@hotmail.com>
2024-07-28 02:00:10 -0300

    modifying headset script

--- Desktop/scripts/g935_battery.sh
+++ Desktop/scripts/g935_battery.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+
 battery_status=$(headsetcontrol -b | grep '%' | awk '{print $2}')
 
-# notify-send "Battery Status" "Headset battery is at: ${battery_status}"
+connection_status=$(headsetcontrol --connected) 
+
+icon="󰋎"
+
+# Chequeo de conexion
+if echo "$connection_status" | grep -q "true"; then
+    color="%{F#00FF00}" 
+elif echo "$connection_status" | grep -q "No supported device found"; then
+    color="%{F#808080}"
+fi
+
+echo "${color}${icon}%{F-} $battery_status"
 
 
+# Para este script es necesario headsetcontrol (github)
